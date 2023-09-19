0498779802c016bbe38e1c45ecddd2543bc17066
Jana <jana.viszkocsilova@gmail.com>
2023-09-19 15:20:06 +0200

    Add files via upload

---
+++ myCv.js
@@ -0,0 +1,51 @@
+let myChart = document.getElementById("myChart").getContext("2d");
+
+let massChart = new Chart(myChart, {
+    type: "doughnut", // bar, horizontalBar, pie, doughnut, radar, polarArea
+    data: {
+        labels: [
+            "TypeScript",
+            "Vue.js 3",
+            "QUASAR",
+            "HTML/CSS",
+            "Storybook",
+            "Git",
+        ],
+        datasets: [
+            {
+                label: "My time",
+                data: [17, 17, 17, 17, 17,17],
+                backgroundColor: [
+                    "#BF9ACA",
+                    "white",
+                    "#D9B8C4",
+                    "#ECCFC3",
+                    "#F49FBC",
+                    "pink",
+                ],
+                fontColor: "black",
+                borderColor: "transparent",
+                hoverBorderWidth: 1,
+                hoverBorderColor: "black",
+            },
+        ],
+    },
+
+    options: {
+        cutout: 55,
+        plugins: {
+            legend: {
+                position: "right",
+                labels: {
+                    padding: 15,
+                    color: "black",
+                    font: {
+                        size: 16,
+                        family: "Catamaran",
+                        color: "black",
+                    },
+                },
+            },
+        },
+    },
+});
